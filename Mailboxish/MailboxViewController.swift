988cee93ed91347030db37baf0b6a904da5e1215
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2015-02-20 22:31:55 -0800

    Animations with single message

--- Mailboxish/MailboxViewController.swift
+++ Mailboxish/MailboxViewController.swift
@@ -13,12 +13,19 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var imageFeed: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var singleMessageView: UIView!
-    
-    var startingMessageCenter: CGPoint!
+    @IBOutlet weak var leftIconImageView: UIImageView!
+    @IBOutlet weak var rightIconImageView: UIImageView!
+    let greenColor = UIColor(red: 116/255, green: 215/255, blue: 104/255, alpha: 1)
+    let redColor = UIColor(red: 233/255, green: 85/255, blue: 59/255, alpha: 1)
+    let yellowColor = UIColor(red: 249/255, green: 209/255, blue: 69/255, alpha: 1)
+    let tanColor = UIColor(red: 215/255, green: 165/255, blue: 120/255, alpha: 1)
+    let grayColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.contentSize = imageFeed.frame.size
+        
         // Do any additional setup after loading the view.
     }
 
@@ -40,13 +47,72 @@ class MailboxViewController: UIViewController {
 
     @IBAction func didPanMessage(sender: UIPanGestureRecognizer) {
         var translation = sender.translationInView(view)
-        var location = sender.locationInView(view)
+        var newMessagePoint = singleMessageView.frame.origin
+        newMessagePoint.x = newMessagePoint.x + translation.x
         
-        if (sender.state == UIGestureRecognizerState.Began) {
-            startingMessageCenter = singleMessageView.center
-        } else if (sender.state == UIGestureRecognizerState.Changed){
-            singleMessageView.center = CGPoint(x: startingMessageCenter.x + translation.x, y: singleMessageView.center.y)
+        if (sender.state == .Began || sender.state == .Changed) {
+            
+            if newMessagePoint.x < -260 {
+                scrollView.backgroundColor = tanColor
+                leftIconImageView.frame.origin.x = newMessagePoint.x - 25 - 17
+                rightIconImageView.frame.origin.x = newMessagePoint.x + 337
+                rightIconImageView.image = UIImage(named: "list_icon")
+            } else if newMessagePoint.x < -60 {
+                scrollView.backgroundColor = yellowColor
+                leftIconImageView.frame.origin.x = newMessagePoint.x - 25 - 17
+                rightIconImageView.frame.origin.x = newMessagePoint.x + 337
+                rightIconImageView.image = UIImage(named: "later_icon")
+            } else if newMessagePoint.x < 0 {
+                leftIconImageView.frame.origin.x = 17
+                scrollView.backgroundColor = grayColor
+            } else if newMessagePoint.x < 60 {
+                rightIconImageView.frame.origin.x = 278
+                scrollView.backgroundColor = grayColor
+            } else if newMessagePoint.x < 260 {
+                scrollView.backgroundColor = greenColor
+                leftIconImageView.frame.origin.x = newMessagePoint.x - 25 - 17
+                rightIconImageView.frame.origin.x = newMessagePoint.x + 337
+                leftIconImageView.image = UIImage(named: "archive_icon")
+            } else {
+                scrollView.backgroundColor = redColor
+                leftIconImageView.frame.origin.x = newMessagePoint.x - 25 - 17
+                rightIconImageView.frame.origin.x = newMessagePoint.x + 337
+                leftIconImageView.image = UIImage(named: "delete_icon")
+            }
+            
+            singleMessageView.frame.origin = newMessagePoint
+            
         } else if (sender.state == UIGestureRecognizerState.Ended) {
+            var targetX = CGFloat(0.0)
+            
+            if newMessagePoint.x <= -60 && newMessagePoint.x > -260 {
+                targetX = singleMessageView.frame.size.width * -1
+            }
+            
+            
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+                    self.singleMessageView.frame.origin.x = targetX
+                }, completion: { (completed: Bool) -> Void in
+                    if newMessagePoint.x <= -60 && newMessagePoint.x > -260 {
+                        self.performSegueWithIdentifier("rescheduleSegue", sender: self)
+                    }
+                } )
+            
+            
+//            if velocity.x > 0 {
+//                println("0")
+//                singleMessageView.frame.origin.x = 220
+//            } else if velocity.x < 0 {
+//                println("middle")
+//                singleMessageView.frame.origin.x = 160
+//            } else if location.x > 60 {
+//                println("location")
+//                singleMessageView.frame.origin.x = 300
+//            } else {
+//                println("x")
+//                singleMessageView.frame.origin.x = startingMessageXPosition + 20
+//            }
         }
+        sender.setTranslation(CGPointZero, inView: view)
     }
 }
