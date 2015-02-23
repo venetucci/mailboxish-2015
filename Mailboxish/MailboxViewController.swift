c9376a48593986532479cc9f3df73dabc2ce7841
Michelle Venetucci Harvey <michelle.ann.harvey@gmail.com>
2015-02-22 18:30:38 -0800

    Updated animations, added pan gesture to open drawer, added readme and gif

--- Mailboxish/MailboxViewController.swift
+++ Mailboxish/MailboxViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MailboxViewController: UIViewController {
 
+    @IBOutlet weak var listView: UIImageView!
     @IBOutlet weak var rescheduleView: UIView!
     @IBOutlet weak var imageFeed: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
@@ -21,18 +22,22 @@ class MailboxViewController: UIViewController {
     let yellowColor = UIColor(red: 249/255, green: 209/255, blue: 69/255, alpha: 1)
     let tanColor = UIColor(red: 215/255, green: 165/255, blue: 120/255, alpha: 1)
     let grayColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1)
+    
+    var scrollViewOpen = false
+    var singleMessageViewRemoved = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.contentSize = imageFeed.frame.size
         rescheduleView.alpha = 0
+        listView.alpha = 0
+        rightIconImageView.alpha = 0
+        leftIconImageView.alpha = 0
         
         var edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: "didPanFromEdge:")
-        edgePan.edges = .Left
+        edgePan.edges = UIRectEdge.Left
         scrollView.addGestureRecognizer(edgePan)
-        
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,8 +77,10 @@ class MailboxViewController: UIViewController {
             } else if newMessagePoint.x < 0 {
                 leftIconImageView.frame.origin.x = 17
                 scrollView.backgroundColor = grayColor
+                rightIconImageView.alpha = newMessagePoint.x/60.0 * -1
             } else if newMessagePoint.x < 60 {
                 rightIconImageView.frame.origin.x = 278
+                leftIconImageView.alpha = newMessagePoint.x/60.0
                 scrollView.backgroundColor = grayColor
             } else if newMessagePoint.x < 260 {
                 scrollView.backgroundColor = greenColor
@@ -92,20 +99,25 @@ class MailboxViewController: UIViewController {
         } else if (sender.state == UIGestureRecognizerState.Ended) {
             var targetX = CGFloat(0.0)
             
-            if newMessagePoint.x <= -60 && newMessagePoint.x > -260 {
+            if newMessagePoint.x <= -60 {
                 targetX = singleMessageView.frame.size.width * -1
             }
             
-            
-                UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
-                    self.singleMessageView.frame.origin.x = targetX
-                }, completion: { (completed: Bool) -> Void in
-                    if newMessagePoint.x <= -60 && newMessagePoint.x > -260 {
-                        
-                            self.rescheduleView.alpha = 1.0
-                        }
-                    } )
-            }
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+                self.singleMessageView.frame.origin.x = targetX
+                self.rightIconImageView.frame.origin.x = targetX + 337
+            }, completion: { (completed: Bool) -> Void in
+                if newMessagePoint.x <= -60 && newMessagePoint.x > -260 {
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        self.rescheduleView.alpha = 1.0
+                    })
+                } else if newMessagePoint.x < -260 {
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        self.listView.alpha = 1.0
+                    })
+                }
+            } )
+        }
         sender.setTranslation(CGPointZero, inView: view)
     }
     
@@ -117,18 +129,78 @@ class MailboxViewController: UIViewController {
         }
     }
     
-    func removeSingleMessageView() {
-        imageFeed.frame.origin.y -= singleMessageView.frame.size.height
-    }
-    
-    func didPanFromEdge(gesture: UIScreenEdgePanGestureRecognizer) {
-        var translation = gesture.translationInView(view)
-        
-        if gesture.state == .Began || gesture.state == .Changed {
-                scrollView.frame.origin.x = translation.x
-        } else if (gesture.state == UIGestureRecognizerState.Ended) {
+    @IBAction func listTapGesture(sender: AnyObject) {
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.listView.alpha = 0.0
+        }) { (completed) -> Void in
+            self.removeSingleMessageView()
         }
     }
     
+    func removeSingleMessageView() {
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.imageFeed.frame.origin.y -= self.singleMessageView.frame.size.height
+        }) { (completed: Bool) -> Void in
+            self.singleMessageViewRemoved = true
+        }
+    }
     
+    func didPanFromEdge(gesture: UIScreenEdgePanGestureRecognizer) {
+        var newScrollViewPoint = scrollView.frame.origin
+        var translation = gesture.translationInView(view)
+        
+        if gesture.state == .Began || gesture.state == .Changed {
+            scrollView.frame.origin.x = translation.x
+        } else if (gesture.state == UIGestureRecognizerState.Ended) {
+            var targetX = CGFloat(0.0)
+            
+            if newScrollViewPoint.x <= 120 {
+                targetX = 0
+            } else {
+                targetX = 300
+            }
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.scrollView.frame.origin.x = targetX
+            }, completion: { (completed: Bool) -> Void in
+                self.scrollViewOpen = true
+            })
+        }
+    }
+    
+    @IBAction func scrollViewOpenPan(sender: UIPanGestureRecognizer) {
+        var newScrollViewPoint = scrollView.frame.origin
+        var translation = sender.translationInView(view)
+        
+        if scrollViewOpen {
+            if (sender.state == UIGestureRecognizerState.Began || sender.state == UIGestureRecognizerState.Changed) {
+                scrollView.frame.origin.x = translation.x + 300
+            } else if (sender.state == UIGestureRecognizerState.Ended) {
+                var targetX = CGFloat(0.0)
+                
+                if newScrollViewPoint.x <= 230 {
+                    targetX = 0
+                    self.scrollViewOpen = false
+                } else {
+                    targetX = 300
+                }
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.scrollView.frame.origin.x = targetX
+                })
+                
+            }
+        }
+    }
+    
+    @IBAction func didPressResetButton(sender: AnyObject) {
+        
+        if singleMessageViewRemoved {
+            imageFeed.frame.origin.y += self.singleMessageView.frame.size.height
+            singleMessageViewRemoved = false
+            singleMessageView.frame.origin.x = 0
+            rightIconImageView.alpha = 0
+            leftIconImageView.alpha = 0
+            leftIconImageView.frame.origin.x = 17
+            rightIconImageView.frame.origin.x = 278
+        }
+    }
 }
